fb405fbb3028dcbecceae6421c482ebdcb482c71
Sam Ballard <sam.ballard@newsela.com>
2023-11-13 15:56:01 -0700

    bg fallback

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -4,11 +4,12 @@ export const theme = extendTheme({
     styles: {
         global: {
             'html': {
-                minH: '100vh',
+                minH: '100%',
                 color: 'cyan.50',
             },
             'body': {
                 color: 'cyan.100',
+                bgColor: 'rgb(10,12,30)',
                 bgImage: "linear-gradient(rgba(0, 0, 0, 0.527),rgba(0, 0, 0, 0.5)), url('/images/bg.jpg')",
                 bgPosition: "center",
                 bgRepeat: "no-repeat",
