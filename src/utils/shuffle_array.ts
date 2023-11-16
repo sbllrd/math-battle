7ba9bbe5dcd302c03082557e05926b080df3dcfb
Sam Ballard <sam.ballard@newsela.com>
2023-11-15 18:35:50 -0700

    fix shuffle array

--- src/utils/shuffle_array.ts
+++ src/utils/shuffle_array.ts
@@ -1,9 +1,10 @@
 const shuffleArray = (array: any[]) => {
-    for (let i = array.length - 1; i > 0; i--) {
+    const newArray = [...array]
+    for (let i = newArray.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+        [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
     }
-    return array
+    return newArray
   }
 
   export default shuffleArray
